5892f02ab71842b7be16aadab9151eb5f73ea599
Salman Javed <mail@salmanjaved.org>
2024-09-07 19:46:40 +1200

    gcc: don't use errno on WinCE
    Refer to https://github.com/MaxKellermann/gcc/commit/499955f5ed123e20fbf07ebb278a4fc0a6fd0be6

--- gcc/tsystem.h
+++ gcc/tsystem.h
@@ -97,11 +97,14 @@ extern void *memset (void *, int, size_t);
 /* All systems have this header.  */
 #include <sys/types.h>
 
+#ifndef UNDER_CE
+
 /* All systems have this header.  */
 #include <errno.h>
 
 #ifndef errno
 extern int errno;
+#endif
 #endif
 
 /* If these system headers do not exist, fixincludes must create them.  */
